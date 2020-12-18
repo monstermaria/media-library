f9e7b2ebb45bb1be63763805cf2b22e6bad6b6be
Maria Andersson <monstermaria@gmail.com>
2020-12-18 09:11:16 +0100

    Added columns and data to the table

--- app/src/main/java/se/monstermaria/medialibrary/MediaLibraryApp.java
+++ app/src/main/java/se/monstermaria/medialibrary/MediaLibraryApp.java
@@ -1,8 +1,11 @@
 package se.monstermaria.medialibrary;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -11,6 +14,8 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import se.monstermaria.medialibrary.model.LibraryModel;
 import se.monstermaria.medialibrary.model.Media;
+
+import java.util.List;
 
 public class MediaLibraryApp extends Application {
 
@@ -48,13 +53,35 @@ public class MediaLibraryApp extends Application {
         // put it all together
         VBox root = new VBox(10, hello, searchAndMenu, mediaTable);
 
-        Scene scene = new Scene(root, 500, 100);
+        Scene scene = new Scene(root, 1100, 500);
         scene.setFill(Color.PINK);
 
         return scene;
     }
 
     private void setupTable(TableView<Media> table) {
+        List<Media> mediaFromModel = model.getMedia();
+        ObservableList<Media> media = FXCollections.observableArrayList(mediaFromModel);
 
+        TableColumn<Media, Integer> idColumn = new TableColumn<>("Media ID");
+        idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+
+        TableColumn<Media, String> typeColumn = new TableColumn<>("Type");
+        typeColumn.setCellValueFactory(new PropertyValueFactory<>("type"));
+
+        TableColumn<Media, String> titleColumn = new TableColumn<>("Title");
+        titleColumn.setCellValueFactory(new PropertyValueFactory<>("title"));
+
+        TableColumn<Media, String> personsColumn = new TableColumn<>("Author/Actors");
+        personsColumn.setCellValueFactory(new PropertyValueFactory<>("personList"));
+
+        TableColumn<Media, Integer> yearColumn = new TableColumn<>("Year");
+        yearColumn.setCellValueFactory(new PropertyValueFactory<>("year"));
+
+        TableColumn<Media, String> availableColumn = new TableColumn<>("Available");
+        availableColumn.setCellValueFactory(new PropertyValueFactory<>("availability"));
+
+        table.setItems(media);
+        table.getColumns().setAll(idColumn, typeColumn, titleColumn, personsColumn, yearColumn, availableColumn);
     }
 }
